bc7fbd02925cb8734df949ac306c11002c7ee3f6
zaenury <adhiimzaenury@gmail.com>
2021-03-31 14:36:10 +0800

    update detail page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:golek_kos/widgets/facility_item.dart';
 
 import '../theme.dart';
 
@@ -40,6 +41,199 @@ class DetailPage extends StatelessWidget {
                 ],
               ),
             ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 328,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: TITLE
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Kuretakeso Hot',
+                                  style: blackTextStyle.copyWith(fontSize: 22),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$52',
+                                    style: purpleTextStyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: ' / month',
+                                        style: greyTextStyle.copyWith(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/icon_star.png',
+                                  width: 20,
+                                  color: Color(0xff989BA1),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: MAIN FACILITIES
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Main Facilities',
+                          style: regulerTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: edge),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                              imageUrl: 'assets/icon_kitchen.png',
+                              number: 2,
+                              facilty: 'kitchen',
+                            ),
+                            FacilityItem(
+                              imageUrl: 'assets/icon_bedroom.png',
+                              number: 3,
+                              facilty: 'bedroom',
+                            ),
+                            FacilityItem(
+                              imageUrl: 'assets/icon_cupboard.png',
+                              number: 3,
+                              facilty: 'Big Lemari',
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: PHOTOS
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Photos',
+                          style: regulerTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Container(
+                        height: 88,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            SizedBox(
+                              width: edge,
+                            ),
+                            Image.asset(
+                              'assets/photo1.png',
+                              height: 88,
+                              width: 110,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 18,
+                            ),
+                            Image.asset(
+                              'assets/photo2.png',
+                              height: 88,
+                              width: 110,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 18,
+                            ),
+                            Image.asset(
+                              'assets/photo3.png',
+                              height: 88,
+                              width: 110,
+                              fit: BoxFit.cover,
+                            ),
+                            SizedBox(
+                              width: 18,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
